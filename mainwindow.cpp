70b9f4fad5621db81eac5b6b8b1613d3b4ad549e
RS <51824085+ruslans-s@users.noreply.github.com>
2019-12-22 01:30:12 +0300

    Update mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -40,14 +40,20 @@ void MainWindow::on_pushButton_clicked()
            model->setData(index, QString::number(infoModelling[row][col]));
         }
     }
-    double tempInt1=0,tempInt2=0,tempInt3=0,tempInt4=0,tempInt5=0,tempInt6=ui->spinBox_4->text().toDouble();
+    double tempInt1=0,tempInt2=0,tempInt3=0,tempInt4=0,tempInt5=0,tempInt6=ui->spinBox_4->text().toDouble(),tempInt7=0;
     for(int i=0;i<infoModelling.size()-1;i=i+60){
         tempInt1+=infoModelling[i][0];
         tempInt2+=infoModelling[i][1];
         tempInt3+=infoModelling[i][2];
-       // tempInt4+=infoModelling[i][3];
+        tempInt5+=infoModelling[i][3];
     }
+    tempInt4=infoModelling[infoModelling.size()-1][3];
+    tempInt7=infoModelling[infoModelling.size()-1][4];
     ui->lineEdit->setText(QString::number(tempInt1/tempInt6));
     ui->lineEdit_2->setText(QString::number(tempInt2/tempInt6));
     ui->lineEdit_3->setText(QString::number(tempInt3/tempInt6));
+    ui->lineEdit_4->setText(QString::number(tempInt5/tempInt6));
+    ui->lineEdit_5->setText(QString::number((tempInt4*ui->spinBox_2->text().toDouble())/(tempInt6*60)));
+    ui->lineEdit_6->setText(QString::number((tempInt7*ui->spinBox_2->text().toDouble())/(tempInt6*60)));
+
 }
